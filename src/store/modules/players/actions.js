0c01aba48052fc5531ad3dc9aff562aed2a4f4b9
Matteo <grossomatteo.lavoro@gmail.com>
2024-04-11 11:23:22 +0200

    prod

--- src/store/modules/players/actions.js
+++ src/store/modules/players/actions.js
@@ -5,7 +5,7 @@ export default {
         }
     
         const response = await fetch(
-          `http://localhost:8080/players`
+          `http://http://ec2-35-174-170-51.compute-1.amazonaws.com:8080/players`
         );
         const responseData = await response.json();
     
