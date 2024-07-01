f570512435401f0bd8a08d01692b22881f3b6ff2
Matteo <grossomatteo.lavoro@gmail.com>
2024-07-01 22:52:17 +0200

    new endpoint - TODO aws param

--- src/store/index.js
+++ src/store/index.js
@@ -12,6 +12,6 @@ const store= createStore({
     },
 })
 
-export const endpoint = 'https://se84ct8bne.execute-api.us-east-1.amazonaws.com/Prod';
+export const endpoint = 'https://tax3k68qj2.execute-api.us-east-1.amazonaws.com/Prod';
 
 export default store;
